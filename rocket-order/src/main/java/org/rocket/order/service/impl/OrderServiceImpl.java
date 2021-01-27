229eab3126a298f7c4c01fa199fb57d26b724329
yangxin <1393003255@qq.com>
2021-01-27 16:46:45 +0800

    feat: 顺序消息局部并行发送实战-2

--- rocket-order/src/main/java/org/rocket/order/service/impl/OrderServiceImpl.java
+++ rocket-order/src/main/java/org/rocket/order/service/impl/OrderServiceImpl.java
@@ -2,27 +2,34 @@ package org.rocket.order.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.common.message.Message;
 import org.rocket.order.constants.OrderStatusEnum;
 import org.rocket.order.entity.Order;
 import org.rocket.order.mapper.OrderMapper;
 import org.rocket.order.service.OrderService;
+import org.rocket.order.service.producer.OrderlyProducer;
+import org.rocket.order.util.FastJsonConvertUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.yangxin.store.service.api.StoreServiceAPI;
 
-import java.util.Date;
-import java.util.UUID;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 /**
  * @author yangxin
  * 12/23/20 4:44 PM
  */
-@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+@SuppressWarnings({"SpringJavaInjectionPointsAutowiringInspection", "SpellCheckingInspection"})
 @Service
 @Slf4j
 public class OrderServiceImpl implements OrderService {
 
+    public static final String PKG_TOPIC = "pkg_topic";
+    public static final String PKG_TAGS = "pkg";
+
     private final OrderMapper orderMapper;
+    private final OrderlyProducer orderlyProducer;
 
     @Reference(
             version = "1.0.0",
@@ -32,11 +39,12 @@ public class OrderServiceImpl implements OrderService {
             timeout = 1000
             // 读请求允许重试3次，写请求不进行重试（如果没做幂等处理）
     )
-    private StoreServiceAPI storeServiceAPI;
+    private StoreServiceAPI storeServiceApi;
 
     @Autowired
-    public OrderServiceImpl(OrderMapper orderMapper) {
+    public OrderServiceImpl(OrderMapper orderMapper, OrderlyProducer orderlyProducer) {
         this.orderMapper = orderMapper;
+        this.orderlyProducer = orderlyProducer;
     }
 
     @Override
@@ -61,8 +69,8 @@ public class OrderServiceImpl implements OrderService {
                 .build();
 
         // 当前版本
-        int currentVersion = storeServiceAPI.selectVersion(supplierId, goodIds);
-        int updateReturnCount = storeServiceAPI.updateStoreCountByVersion(currentVersion,
+        int currentVersion = storeServiceApi.selectVersion(supplierId, goodIds);
+        int updateReturnCount = storeServiceApi.updateStoreCountByVersion(currentVersion,
                 supplierId,
                 goodIds,
                 "admin",
@@ -77,7 +85,7 @@ public class OrderServiceImpl implements OrderService {
             flag = false;
 
             // 没有更新成功，1：高并发时乐观锁生效；2：库存不足
-            int currentStoreCount = storeServiceAPI.selectStoreCount(supplierId, goodIds);
+            int currentStoreCount = storeServiceApi.selectStoreCount(supplierId, goodIds);
             if (currentStoreCount == 0) {
                 // {flag:false, messageCode:xxx, message: "当前库存不足"}
                 log.info("当前库存不足……");
@@ -88,5 +96,44 @@ public class OrderServiceImpl implements OrderService {
         }
 
         return flag;
+    }
+
+    @Override
+    public void sendOrderlyMessage4Pkg(String userId, String orderId) {
+        List<Message> messageList = new ArrayList<>();
+
+        Message message1 = buildOrderlyMessage4Pkg(userId, orderId, "创建包裹操作1");
+        messageList.add(message1);
+
+        Message message2 = buildOrderlyMessage4Pkg(userId, orderId, "发送物流通知操作2");
+        messageList.add(message2);
+
+        // 顺序消息投递是应该按照供应商Id与topic和messagequeueId进行绑定对应的
+        // supplier_id
+        Order order = orderMapper.selectByPrimaryKey(orderId);
+        int messageQueueNumber = Integer.parseInt(order.getSupplierId());
+
+        // 对应的顺序消息的生产者把messageList发出去
+        orderlyProducer.sendOrderlyMessages(messageList, messageQueueNumber);
+    }
+
+    private Message buildOrderlyMessage4Pkg(String userId, String orderId, String text) {
+        Map<String, Object> paramMap = buildOrderLyMessageMap(userId, orderId, text);
+        String json = FastJsonConvertUtil.convertObject2Json(paramMap);
+        if (json == null) {
+            return null;
+        }
+
+        String key = UUID.randomUUID().toString() + "$" + System.currentTimeMillis();
+        return new Message(PKG_TOPIC, PKG_TAGS, key, json.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private Map<String, Object> buildOrderLyMessageMap(String userId, String orderId, String text) {
+        Map<String, Object> paramMap = new HashMap<>(4);
+        paramMap.put("userId", userId);
+        paramMap.put("orderId", orderId);
+        paramMap.put("text", text);
+
+        return paramMap;
     }
 }
---
+++ rocket-order/src/main/java/org/rocket/order/service/producer/OrderlyProducer.java
@@ -0,0 +1,58 @@
+package org.rocket.order.service.producer;
+
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author yangxin
+ * 1/27/21 2:58 PM
+ */
+@SuppressWarnings("SpellCheckingInspection")
+@Component
+public class OrderlyProducer {
+
+    private DefaultMQProducer producer;
+
+    private static final String NAMESRV_ADDR_SINGLE = "192.168.3.2:9876";
+    public static final String PRODUCER_GROUP_NAME = "orderly_producer_group_name";
+
+    private OrderlyProducer() {
+        this.producer = new DefaultMQProducer(PRODUCER_GROUP_NAME);
+        this.producer.setNamesrvAddr(NAMESRV_ADDR_SINGLE);
+        this.producer.setSendMsgTimeout(3000);
+        start();
+    }
+
+    public void start() {
+        try {
+            producer.start();
+        } catch (MQClientException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void shutdown() {
+        producer.shutdown();
+    }
+
+    public void sendOrderlyMessages(List<Message> messageList, int messageQueueNumber) {
+        for (Message message : messageList) {
+            try {
+                producer.send(message, (mqs, msg, arg) -> {
+                    int id = (int) arg;
+                    return mqs.get(id);
+                }, messageQueueNumber);
+            } catch (MQClientException | RemotingException | MQBrokerException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
